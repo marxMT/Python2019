79a62325e6d96afe99fc183b678a588536f5232c
Mariano Otero <mariano.otero@moni.com.ar>
2019-05-26 19:45:31 -0300

    maneja exepciones al escribir o leer de un archivo que no existe

--- consumiendoJson.py
+++ consumiendoJson.py
@@ -1,6 +1,6 @@
 import json
 import csv
-# import os
+import os
 
 
 def leerJson(archivo):
@@ -8,8 +8,11 @@ def leerJson(archivo):
     value: obj Json
     return: python obj
     """
-    with open(archivo) as f:
-        data = json.load(f)
+    try:
+        with open(archivo) as f:
+            data = json.load(f)
+    except OSError as err:
+        print("OS error: {0}".format(err))
     return data
 
 
@@ -18,11 +21,14 @@ def leerCsv(archivo):
     value: Obj csv
     return: Python List
     """
-    lista_csv=[]
-    with open('people.csv', 'r') as csvFile:
-        reader = csv.reader(csvFile)
-        for fila in reader:
-            lista_csv.append(fila)
+    lista_csv = []
+    try:
+        with open('people.csv', 'r') as csvFile:
+            reader = csv.reader(csvFile)
+            for fila in reader:
+                lista_csv.append(fila)
+    except OSError as err:
+        print("OS error: {0}".format(err))
     return lista_csv
 
 
@@ -32,18 +38,24 @@ def convertirToCsv(data_python):
     """
     columns = list(data_python[0].keys())
 
-    with open('data.csv', 'w') as archivoCsv:
-        arch_csv = csv.DictWriter(archivoCsv, fieldnames=columns)
-        arch_csv.writeheader()
-        arch_csv.writerows(data_python)
+    try:
+        with open('data.csv', 'w') as archivoCsv:
+            arch_csv = csv.DictWriter(archivoCsv, fieldnames=columns)
+            arch_csv.writeheader()
+            arch_csv.writerows(data_python)
+    except OSError as err:
+        print("OS error: {0}".format(err))
 
 
 def convertirToJson(data_python):
     """
     Toma obj Python y genera un archivo Json
     """
-    with open('data.json', 'w') as archivoJson:
-        json.dump(data_python, archivoJson)
+    try:
+        with open('data.json', 'w') as archivoJson:
+            json.dump(data_python, archivoJson)
+    except OSError as err:
+        print("OS error: {0}".format(err))
 
 
 def generarElementosTabla(data_json):
